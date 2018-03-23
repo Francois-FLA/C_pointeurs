096ee07587c14ce315c88dde445e56ddb579dd18
surnova <francois.laffon.dev@gmail.com>
2018-03-23 15:34:09 +0100

    premier commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void decoupMinute (int* nombreHeures, int* nombreMinutes );
+
+
+int main (int argc, char *argv[])
+{
+    int heures = 0, minutes = 0;
+
+    printf("Veuillez entrer le nombre de minutes a convertir\n");
+    scanf("%d", &minutes);
+
+    decoupMinute(&heures, &minutes);
+
+    printf("Il y a donc %d heures et %d minutes\n", heures, minutes);
+
+
+    return 0;
+}
+
+void decoupMinute (int* nombreHeures, int* nombreMinutes )
+{
+    *nombreHeures = *nombreMinutes / 60;
+    *nombreMinutes = *nombreMinutes % 60;
+}
